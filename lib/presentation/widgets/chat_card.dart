facd242175bb10967bad491d9be83a8b50a52452
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-05-14 03:40:56 +0200

    preview all information of user in the card

--- lib/presentation/widgets/chat_card.dart
+++ lib/presentation/widgets/chat_card.dart
@@ -2,8 +2,12 @@ import 'package:chat_app/core/media_query.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../data/models/chat_user.dart';
+
 class ChatCard extends StatelessWidget {
-  const ChatCard({Key? key}) : super(key: key);
+  const ChatCard({Key? key, required this.user}) : super(key: key);
+
+  final ChatUser user;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +16,11 @@ class ChatCard extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: mediaQuery(context).width * .01 , vertical: 4),
       child:  ListTile(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        title: const Text("Contact Name"),
-        subtitle: const Text("Last Message"),
-        trailing: const Text("7:00 AM"),
-        leading: const CircleAvatar(
-          child: Icon(CupertinoIcons.person_2_alt),
+        title:  Text(user.name ?? "Name"),
+        subtitle:  Text(user.about ?? "About"),
+        trailing:  Text(user.lastActive ?? "7:00 AM"),
+        leading:  const CircleAvatar(
+          child:  Icon(CupertinoIcons.person_2_alt),
         ),
       ),
     );
