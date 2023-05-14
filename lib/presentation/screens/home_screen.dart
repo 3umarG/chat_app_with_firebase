0e0d987d5bee1a21e262853a3e5404687c5cd470
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-05-14 03:40:30 +0200

    handle the empty data from the database

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:chat_app/business/chats/chats_cubit.dart';
 import 'package:chat_app/data/models/chat_user.dart';
 import 'package:chat_app/presentation/widgets/chat_card.dart';
@@ -6,6 +5,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/constants/routes.dart';
+import '../../core/media_query.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -33,7 +35,15 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: const Icon(CupertinoIcons.home),
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+          IconButton(
+            onPressed: (){
+              Navigator.pushReplacementNamed(
+                context,
+                AppRoutes.loginScreenRoute,
+              );
+            },
+            icon: const Icon(Icons.person),
+          ),
         ],
       ),
       floatingActionButton: Padding(
@@ -43,43 +53,69 @@ class _HomeScreenState extends State<HomeScreen> {
           child: const Icon(Icons.chat_outlined),
         ),
       ),
-      body: BlocConsumer<ChatsCubit, ChatsState>(
-        listener: (context, state) {
-          // TODO: implement listener
-        },
-        builder: (context, state) {
-          return StreamBuilder(
-              stream: context
-                  .read<ChatsCubit>()
-                  .firestore
-                  .collection("users")
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasData) {
-                  final List<ChatUser> listOfUsers = List<ChatUser>.from(
-                      snapshot.data!.docs
-                          .map((e) => ChatUser.fromJson(e.data())));
-                  debugPrint(listOfUsers[0].name.toString());
-                  return ListView.builder(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    itemBuilder: (context, index) => const ChatCard(),
-                    itemCount: listOfUsers.length,
-                    physics: const BouncingScrollPhysics(),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text(snapshot.error!.toString()),
-                  );
-                } else {
-                  return const SizedBox.shrink();
-                }
-              });
-        },
-      ),
+      body: StreamBuilder(
+          stream: context
+              .read<ChatsCubit>()
+              .firestore
+              .collection("users")
+              .snapshots(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.active:
+              case ConnectionState.done:
+                final List<ChatUser> listOfUsers = List<ChatUser>.from(
+                    snapshot
+                        .data!.docs
+                        .map((e) => ChatUser.fromJson(e.data())));
+                return listOfUsers.isNotEmpty
+                    ? ListView.builder(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  itemBuilder: (context, index) =>
+                      ChatCard(
+                        user: listOfUsers[index],
+                      ),
+                  itemCount: listOfUsers.length,
+                  physics: const BouncingScrollPhysics(),
+                )
+                    : Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/empty.png",
+                        width: mediaQuery(context).width * .5,
+                        height: mediaQuery(context).height * .5,
+                      ),
+                      const Text(
+                        "There is no Chats , yet !!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+            }
+            // if (snapshot.connectionState == ConnectionState.waiting) {
+            // } else if (snapshot.hasData) {
+            //
+            // } else if (snapshot.hasError) {
+            //   return Center(
+            //     child: Text(snapshot.error!.toString()),
+            //   );
+            // } else {
+            //   return const SizedBox.shrink();
+            // }
+          }),
     );
   }
 }
